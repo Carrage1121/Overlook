6dd8d4b172381a4b811720a7dafa415986483aca
Carrage1121 <b1329955875@163.com>
2022-12-23 15:05:46 +0800

    first push

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,18 @@
+#pragma once
+
+//For use by Hazel application
+
+#include <stdio.h>
+#include "Hazel/Application.h"
+#include "Hazel/Layer.h"
+#include "Hazel/Log.h"
+
+#include "Hazel/Input.h"
+#include "Hazel/KeyCodes.h"
+#include "Hazel/MouseButtonCodes.h"
+
+#include "Hazel/ImGui/ImGuiLayer.h"
+
+//----------Entry Point-----------
+#include "Hazel/EntryPoint.h"
+//--------------------------------
